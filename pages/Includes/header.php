901703a05aa574bc0f80dc88189285efd86c588a
gaura2003 <gauravprajapat2305@gmail.com>
2024-03-12 18:23:25 +0530

    update

---
+++ pages/Includes/header.php
@@ -0,0 +1,24 @@
+<?php 
+echo '<html>
+  <body>
+    <header>
+        <div class="logo-holder">
+       <a><img src="../images\music app logo.jpg" alt=""></a>
+        </div>
+        <div class="header-div">
+            <div class="main-title">
+                Web Music App
+            </div>
+             <div class="footer-div">
+            <form action="">
+                <div class="form-group">
+                    <input class="form-control" type="text" placeholder="Search for Music " name="find" id="">
+                    <button class="btn">Search</button>                
+                </div>
+            </form>
+        </div>            
+           </header>
+  </body>  
+</html>
+'
+?>
